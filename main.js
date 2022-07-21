2db216941b9b019054b002e068c020be6cdb6121
Advit <77945990+advitarora135@users.noreply.github.com>
2022-07-21 13:16:59 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+captured_img="";
 prediction_1="";
 prediction_2="";
 
@@ -5,7 +6,7 @@ Webcam.set({
     width: 350,
     hieght: 300,
     image_format: 'png',
-    png_quality: 90
+    png_quality: 100
 });
 camera = document.getElementById("camera");
 
@@ -14,10 +15,11 @@ Webcam.attach("#camera");
 function take_snapshot() {
     Webcam.snap(function (data_uri) {
         document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+        captured_img=data_uri;
     });
 }
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5plhn1WlL/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/gRYDa_o4W/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log('Model Loaded!');
@@ -29,3 +31,46 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
 }
+function check()
+{
+  img = document.getElementById('captured_image');
+  classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_emotion_name").innerHTML = results[0].label;
+      document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+      prediction_1 = results[0].label;
+      prediction_2 = results[1].label;
+      speak();
+      if(results[0].label == "Amazing")
+      {
+          document.getElementById("update_emoji").innerHTML = "&#128076;";
+      }
+      if(results[0].label == "Nice Job")
+      {
+          document.getElementById("update_emoji").innerHTML = "&#128077;";
+      }
+      if(results[0].label == "Victory")
+      {
+          document.getElementById("update_emoji").innerHTML = "&#9996;";
+      }
+  
+      if(results[1].label == "Amazing")
+      {
+          document.getElementById("update_emoji2").innerHTML = "&#128076;";
+      }
+      if(results[1].label == "Nice Job")
+      {
+          document.getElementById("update_emoji2").innerHTML = "&#128077;";
+      }
+      if(results[1].label == "Victory")
+      {
+          document.getElementById("update_emoji2").innerHTML = "&#9996;";
+      }
+    }
+  }
+  
